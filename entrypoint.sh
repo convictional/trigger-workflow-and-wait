728dd0f165e6ee924655cd64216fd5718185224b
Shabih <81912045+shabih-paystack@users.noreply.github.com>
2021-07-14 20:20:07 +0400

    Add echo statement

--- entrypoint.sh
+++ entrypoint.sh
@@ -96,7 +96,8 @@ wait_for_workflow_to_finish() {
     -H "Authorization: Bearer ${INPUT_GITHUB_TOKEN}" | jq '[.workflow_runs[]] | first')
   last_workflow_id=$(echo "${last_workflow}" | jq '.id')
   echo "The workflow id is [${last_workflow_id}]."
-  echo "::set-output name=workflow_id::$last_workflow_id"
+  echo "The workflow logs can be found at https://github.com/${INPUT_OWNER}/${INPUT_REPO}/actions/runs/${last_workflow_id}"
+  echo "::set-output name=workflow_id::${last_workflow_id}"
   echo ""
   conclusion=$(echo "${last_workflow}" | jq '.conclusion')
   status=$(echo "${last_workflow}" | jq '.status')
