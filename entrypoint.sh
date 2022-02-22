9c8fc493208e283d1d8674ea4a4aa4faca880b99
Neil Mayhew <neil@mayhew.name>
2022-02-21 18:44:09 -0700

    Remove extraneous single quotes

--- entrypoint.sh
+++ entrypoint.sh
@@ -110,7 +110,7 @@ api() {
 get_workflow_runs() {
   since=${1:?}
 
-  query="event=workflow_dispatch&created=>=$since${INPUT_GITHUB_USER+'&actor='}${INPUT_GITHUB_USER}&per_page=100"
+  query="event=workflow_dispatch&created=>=$since${INPUT_GITHUB_USER+&actor=}${INPUT_GITHUB_USER}&per_page=100"
 
   echo "Getting workflow runs using query: ${query}" >&2
 
